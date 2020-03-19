83bad9181568b27b58a4b5758275738e387fbadb
Aleksipa <paavola.aleksi@gmail.com>
2020-03-19 22:51:59 +0200

    subscription to course added

--- application/courses/views.py
+++ application/courses/views.py
@@ -10,6 +10,15 @@ def courses_index():
 def courses_form():
     return render_template("courses/new.html")
 
+@app.route("/courses/<course_id>/", methods=["POST"])
+def courses_set_done(course_id):
+
+    t = Course.query.get(course_id)
+    t.done = True
+    db.session().commit()
+  
+    return redirect(url_for("courses_index"))
+
 @app.route("/courses/", methods=["POST"])
 def courses_create():
     t = Course(request.form.get("name"))
