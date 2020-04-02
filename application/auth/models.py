32c896ada4ea2c7120c0cf828599467f1de771e7
Aleksipa <paavola.aleksi@gmail.com>
2020-04-02 14:13:58 +0300

    user listing added

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,33 +1,41 @@
 from application import db
+from application.models import Base
 
-class User(db.Model):
+from sqlalchemy.sql import text
+
+class User(Base):
 
     __tablename__ = "account"
-  
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
 
     tasks = db.relationship("Course", backref='account', lazy=True)
-
-    def __init__(self, name, username, password):
-        self.name = name
-        self.username = username
-        self.password = password
   
-    def get_id(self):
-        return self.id
+    def get_id(self, name):
+         self.name = name
 
     def is_active(self):
-        return True
+        return self.id
 
     def is_anonymous(self):
         return False
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def find_users_with_no_subscription(done=0):
+        stmt = text("SELECT Account.id, Account.name FROM Account"
+                     " LEFT JOIN Course ON Course.account_id = Account.id"
+                     " WHERE (Course.done IS null OR Course.done = :done)"
+                     " GROUP BY Account.id"
+                     " HAVING COUNT(Course.id) = 0").params(done=done)
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})
+
+        return response
